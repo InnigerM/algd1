180589bb0128ee66ab8c9e4b3d86c8d0559ca908
Marco <marco.inniger1@students.fhnw.ch>
2018-10-10 17:26:39 +0200

    Added sentinelSearchFirstElement

--- search/src/main/java/ch/fhnw/algd1/search/sentinelsearch/SentinelSearch.java
+++ search/src/main/java/ch/fhnw/algd1/search/sentinelsearch/SentinelSearch.java
@@ -1,20 +1,30 @@
 package ch.fhnw.algd1.search.sentinelsearch;
 
 public class SentinelSearch {
-	public static boolean exists(int[] data, int value) {
-		int lastValue = data[data.length - 1], i = 0;
-		data[data.length - 1] = value;
+    public static boolean exists(int[] data, int value) {
+        int lastValue = data[data.length - 1], i = 0;
+        data[data.length - 1] = value;
 
-		while(data[i] != value){
-			i++;
-		}
+        while (data[i] != value) {
+            i++;
+        }
 
-		data[data.length - 1] = lastValue;
-		return (i != data.length - 1 || lastValue == value);
-	}
+        data[data.length - 1] = lastValue;
+        return (i != data.length - 1 || lastValue == value);
+    }
 
-	public static int firstIndex(int[] data, int value) {
-		// TODO (A3 extra) Find first occurrence on an element in data (advanced challenge)
-		return -1;
-	}
+    public static int firstIndex(int[] data, int value) {
+        int lastValue = data[data.length - 1], i = 0;
+        data[data.length - 1] = value;
+
+        while (data[i] != value) {
+            i++;
+        }
+        data[data.length - 1] = lastValue;
+        if (i == data.length - 1 && value != lastValue) {
+            return -1;
+        } else {
+            return i;
+        }
+    }
 }
