93cf9b398d8ccd1fe39b39eaf4757736898fd7ac
Marco <marco.inniger1@students.fhnw.ch>
2018-10-10 09:21:56 +0200

    Deletion commit: Moved files to different locations

--- src/main/java/ch/fhnw/algd1/search/binsearchfirst/BinSearchFirstElement.java
+++
@@ -1,8 +0,0 @@
-package ch.fhnw.algd1.search.binsearchfirst;
-
-public class BinSearchFirstElement {
-	public static int binSearch(int[] data, int value) {
-		// TODO (A7) Implement bianry search for first element
-		return 0;
-	}
-}
