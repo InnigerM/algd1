e16f425613fa011f28622dee0ebef18c4c394f25
InnigerM <marco.inniger@gmail.com>
2019-01-06 20:52:48 +0100

    Added and tested MergeSort

--- mergesort/src/main/java/ch/fhnw/algd1/mergesort/Mergesort.java
+++ mergesort/src/main/java/ch/fhnw/algd1/mergesort/Mergesort.java
@@ -1,19 +1,33 @@
 package ch.fhnw.algd1.mergesort;
 
 public class Mergesort {
-	public <T extends Comparable<? super T>> void mergesort(T[] a) {
-		// TODO implement header method to start recursive sorting
-	}
+    public <T extends Comparable<? super T>> void mergesort(T[] a) {
+        sort(a, 0, a.length);
+    }
 
-	private <T extends Comparable<? super T>> void sort(T[] a, int beg, int end) {
-		// TODO implement recursive sorting with merge sort strategy
-	}
+    private <T extends Comparable<? super T>> void sort(T[] a, int beg, int end) {
+        if (end - beg > 1) {
+            int m = (beg + end) / 2;
+            sort(a, beg, m);
+            sort(a, m, end);
+            merge(a, beg, m, end);
+        }
+    }
 
-	// Merges sequence a[beg...m-1] with a[m...end-1] into a[beg...end-1]
-	private <T extends Comparable<? super T>> void merge(T[] a, int beg, int m, int end) {
-		@SuppressWarnings("unchecked")
-		T[] b = (T[])new Comparable[end - beg];
-		// TODO copy elements from a[beg...m-1] and a[m...end-1] to b[0...]
-		// TODO copy all elements from b[0...] to a[beg...]
-	}
+    // Merges sequence a[beg...m-1] with a[m...end-1] into a[beg...end-1]
+    private <T extends Comparable<? super T>> void merge(T[] a, int beg, int m, int end) {
+        @SuppressWarnings("unchecked")
+        T[] b = (T[]) new Comparable[end - beg];
+        int i = 0, j = beg, k = m;
+        while (j < m && k < end) {
+            if (a[j].compareTo(a[k]) <= 0) b[i++] = a[j++];
+            else b[i++] = a[k++];
+        }
+        while (j < m) b[i++] = a[j++];
+        while (k < end) b[i++] = a[k++];
+        while(i > 0){
+            i--;
+            a[beg + i] = b[i];
+        }
+    }
 }
